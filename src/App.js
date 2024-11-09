7be494c17f75d7f0811c124d69917536aa8f8bde
unknown <somrathumayan@gmail.com>
2024-11-09 20:49:43 +0600

    online code editor with react, html, css, js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Editor from './components/Editor';
 
 function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(()=>{
+
+    const timeout = setTimeout(()=>{
+       setSrcDoc (
+        `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `
+       ) 
+    }, 250)
+
+    return () => clearTimeout(timeout)
+    
+  }, [html, css, js])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor 
+        language = 'xml' 
+        displayName='HTML'
+        value={html}
+        onChange={setHtml}
+      />
+      <Editor 
+        language = 'css' 
+        displayName='CSS'
+        value={css}
+        onChange={setCss}
+      />
+      <Editor 
+        language = 'javascript' 
+        displayName='JS'
+        value={js}
+        onChange={setJs}
+      />
+      
+      <div className="pane top-pane"></div>
+      <div className="pane">
+        <iframe 
+          srcDoc={srcDoc}
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder=''
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
